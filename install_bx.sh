226bedf33d6dfd63160a06c1003eee66d70a1308
Espen Thilesen <ethilesen@mac.com>
2018-08-10 11:45:23 +0200

    path...

--- install_bx.sh
+++ install_bx.sh
@@ -3,8 +3,10 @@
 
 echo "Download IBMCloud CLI"
 curl -sL https://ibm.biz/idt-installer | bash
-export PATH="/usr/local/bin/:$PATH"
+
 echo $PATH
 echo 'testing cli '
+bx -v
 ibmcloud -v
 ibmcloud plugin list
+kubectl version
