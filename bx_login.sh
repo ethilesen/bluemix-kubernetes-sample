e95abb19d1bfbbe35fad92f7d0816f9e00460176
Espen Thilesen <ethilesen@mac.com>
2018-08-10 10:53:39 +0200

    upgrade ibmcloud cli

--- bx_login.sh
+++ bx_login.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
 if [ -z $CF_ORG ]; then
-  CF_ORG="$BLUEMIX_ORG"
+  API_ENDPOINT="$IBMCLOUD_API_ENDPOINT"
 fi
 if [ -z $CF_SPACE ]; then
-  CF_SPACE="$BLUEMIX_SPACE"
+  API_KEY="$IBMCLOUD_APIKEY"
 fi
 
 
-if [ -z "$BLUEMIX_USER" ] || [ -z "$BLUEMIX_PASSWORD" ] || [ -z "$BLUEMIX_ACCOUNT" ]; then
-  echo "Define all required environment variables and rerun the stage."
-  exit 1
-fi
-echo "Deploy pods"
-
-echo "bx login -a $CF_TARGET_URL"
-bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
+echo "ibmcloud login -a $CF_TARGET_URL"
+ibmcloud login -a "$API_ENDPOINT" --apikey "$APIKEY"
 if [ $? -ne 0 ]; then
   echo "Failed to authenticate to Bluemix"
   exit 1
